0abe297906b7f819f26bb59c1f9d4f426c04826b
tokunaga <ikimono.polka00@gmail.com>
2020-03-09 11:29:20 +0000

    課題作成

---
+++ practice.php
@@ -0,0 +1,14 @@
+<?php
+echo 'hello php!';
+$plus = 10;
+++$plus;
+echo $plus;
+$value = 10;
+$result = $value == 20;
+var_dump($result);
+$a = 20;
+$b = "20";
+$result = $a == $b;
+var_dump($result);
+$result = $a === $b;
+var_dump($result);
