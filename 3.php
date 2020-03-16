a184f896cf787cb45b3fb9b05f8839dd16d4796c
tokunaga <ikimono.polka00@gmail.com>
2020-03-16 06:28:45 +0000

    課題作成

---
+++ 3.php
@@ -0,0 +1,26 @@
+<?php
+$name = "Asahi";
+if ($name = "Asahi") {
+    echo "私は 徳永朝日 です。";
+} else {
+    echo "徳永朝日ではありません。";
+}
+echo "\n";
+$total = 0;
+for ($i = 1; $i <= 1000; $i++) {
+    $total += $i;
+}
+echo $total . "\n";
+$fruits = array("apple", "banana", "strawberry", "grape", "pineapple");
+foreach ($fruits as $fruit) {
+    echo "要素は" . $fruit;
+    echo "\n";
+}
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i++) {
+    if ($i % 5 == 0) {
+        echo $i;
+        echo "\n";
+    }
+}
--- practice.php
+++ practice.php
@@ -1,14 +1,43 @@
 <?php
-echo 'hello php!';
-$plus = 10;
-++$plus;
-echo $plus;
-$value = 10;
-$result = $value == 20;
-var_dump($result);
-$a = 20;
-$b = "20";
-$result = $a == $b;
-var_dump($result);
-$result = $a === $b;
-var_dump($result);
+$height = 230;
+if ($height < 150) {
+    echo "150cm未満の方は乗車できません。";
+} elseif ($height >= 200) {
+    echo "200cm以上の方は乗車できません。";
+} else {
+    echo "乗車できます。";
+}
+$weekday="Thursday";
+switch ($weekday) {
+    case "Monday":
+    case "Thursday":
+        echo "可燃ごみの日です。";
+        break;
+    case "Wednesday":
+        echo"不燃ごみの日です。";
+        break;
+    default:
+        echo"回収はありません。";
+        break;
+}
+for ($i = 0; $i < 10; $i++) {
+    echo $i;
+}
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 100; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+$fruit = array("apple", "orange", "peach");
+echo count($fruit);
+for ($i = 0; $i < count($fruit); $i++) {
+    echo "要素は" . $fruit[$i];
+    echo "\n";
+}
+$animals = array("dog", "cat", "panda");
+foreach ($animals as $animal) {
+    echo "要素は" . $animal;
+    echo "\n";
+}
